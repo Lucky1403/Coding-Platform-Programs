a3381d02a19267bb1e92f49bef8f87a0a7548e48
Lucky1403 <lucky.230062289@gmail.com>
2026-07-09 12:40:52 +0530

    Solved 268 Missing Number Leetcode

---
+++ 268_MissingNumber.cpp
@@ -0,0 +1,22 @@
+#include<vector>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] != i)
+            {
+                return i;
+            }
+        }
+        return n;
+    }
+};
