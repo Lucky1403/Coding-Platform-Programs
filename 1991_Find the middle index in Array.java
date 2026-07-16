60c26551b091c3601e9494c8d1f967613c66a130
Lucky1403 <lucky.230062289@gmail.com>
2026-07-17 02:06:49 +0530

    Solved Find the middle index in Array Leetcode Problem

---
+++ 1991_Find the middle index in Array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int totalSum = 0;
+        int index = -1;
+
+        int prefixSum = 0;
+        int suffixSum = 0;
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            totalSum += nums[i];
+        }
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            suffixSum = totalSum - prefixSum - nums[i];
+
+            if(prefixSum == suffixSum)
+            {
+                index = i;
+                return index;
+            }
+
+            prefixSum = prefixSum + nums[i];
+        }
+
+        return index;
+    }
+}
