9dac3add21dfcbadfe78e8936b4f9dcad332e745
Lucky1403 <lucky.230062289@gmail.com>
2026-07-17 17:45:13 +0530

    Solved Transpose Matrix Leetcode Problem

---
+++ 867_Transpose Matrix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int result[][] = new int[columns][rows];
+
+        for(int i = 0; i < columns; i++)
+        {
+            for(int j = 0; j < rows; j++)
+            {
+                result[i][j] = matrix[j][i];
+            }
+        }
+        return result;
+    }
+}
