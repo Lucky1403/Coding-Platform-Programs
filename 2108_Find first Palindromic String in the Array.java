81a9aba7ddfc2f39a4da2faf91ee81d7e1859f9c
Lucky1403 <lucky.230062289@gmail.com>
2026-07-22 01:27:26 +0530

    Solved Find First Palindromic String in the Array Leetcode Problem

---
+++ 2108_Find first Palindromic String in the Array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPalindrome(String str)
+    {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while(left < right)
+        {
+            if(str.charAt(left) != str.charAt(right))
+            {
+                return false;
+            }
+            else
+            {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+    public String firstPalindrome(String[] words) {
+        
+        for(String str : words)
+        {
+            if(isPalindrome(str))
+            {
+                return str;
+            }
+        }
+
+        return "";
+    }
+}
