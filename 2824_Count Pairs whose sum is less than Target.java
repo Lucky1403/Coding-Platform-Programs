ff780845da3cbc9f5487e1a3cf367e6207065fcd
Lucky1403 <lucky.230062289@gmail.com>
2026-07-23 22:47:51 +0530

    Solved Count Pairs whose sum is less that Target Leetcode Problem

---
+++ 2824_Count Pairs whose sum is less than Target.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int countPairs(List<Integer> nums, int target) {
+        int output = 0;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            for(int j = i + 1 ; j < nums.size(); j++)
+            {
+                if(nums.get(i) + nums.get(j) < target)
+                {
+                    output++;
+                }
+            }
+        }
+
+        return output;
+    }
+}
