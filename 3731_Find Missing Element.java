c4ae24665fb4cd0ba6b74e48b04304cbbe13fd99
Lucky1403 <lucky.230062289@gmail.com>
2026-08-03 14:04:38 +0530

    Solved Find Missing Element Leetcode Problem

---
+++ 3731_Find Missing Element.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+import java.util.ArrayList;
+
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        
+        if(nums == null || nums.length == 0)
+        {
+            return new ArrayList<>();
+        }
+        HashSet<Integer> set = new HashSet<>();
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] > max)
+            {
+                max = nums[i];
+            }
+
+            if(nums[i] < min)
+            {
+                min = nums[i];
+            }
+
+            set.add(nums[i]);
+        }
+
+        ArrayList<Integer> l1 = new ArrayList<>();
+
+        for(int i = min; i < max; i++)
+        {
+            if(!set.contains(i))
+            {
+                l1.add(i);
+            }
+        }
+
+        return l1;
+    }
+}
