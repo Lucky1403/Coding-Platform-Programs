57931f5a25eb0870807360c636dfaf19bc0d9a9a
Lucky1403 <lucky.230062289@gmail.com>
2026-08-10 14:07:06 +0530

    olved Most Frequent Character GFG Problem

---
+++ Most Frequenct Character.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+
+class Solution {
+    public static char getMaxOccuringChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            if(map.containsKey(ch)){
+                int freq = map.get(ch);
+                map.put(ch, freq + 1);
+            }
+            else
+            {
+                map.put(ch, 1);
+            }
+        }
+        
+        int maxFrequency = 0;
+        for(char ch : map.keySet())
+        {
+            int freq = map.get(ch);
+            if(freq > maxFrequency)
+            {
+                maxFrequency = freq;
+            }
+        }
+        
+        char ans = 'z';
+        for(char ch : map.keySet())
+        {
+            int freq = map.get(ch);
+            if(freq == maxFrequency && ch < ans)
+            {
+                ans = ch;
+            }
+        }
+        
+        return ans;
+    }
+}
