411f7e96687893fb406bb669750f4d7bd60bbc35
Lucky1403 <lucky.230062289@gmail.com>
2026-08-11 16:58:53 +0530

    Solved First Occurence in Sorted GFG Problem

---
+++ First Occurence in Sorted.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int firstSearch(int[] arr, int k) {
+        int low = 0;
+        int high = arr.length - 1;
+        int index = -1;
+        
+        while(low <= high)
+        {
+            int mid = (low + high)/2;
+            
+            if(arr[mid] == k)
+            {
+                index = mid;
+                high = mid - 1;
+            }
+            else if(arr[mid] > k)
+            {
+                high = mid - 1;
+            }
+            else if(arr[mid] < k)
+            {
+                low = mid + 1;
+            }
+        }
+        
+        return index;
+    }
+}
