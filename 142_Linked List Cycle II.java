cadcf36d821619021effc1172770f8fa1bab69cd
Lucky1403 <lucky.230062289@gmail.com>
2026-08-13 21:50:46 +0530

    Solved Linked List Cycle II Leetcode Problem

---
+++ 142_Linked List Cycle II.java
@@ -0,0 +1,33 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!= null && fast.next!= null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+            {
+                ListNode dummy = head;
+                while(slow != dummy)
+                {
+                    dummy = dummy.next;
+                    slow = slow.next;
+                }
+                return dummy;
+            }
+        }
+        return null;
+    }
+}
