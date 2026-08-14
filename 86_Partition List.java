0234056fbdb60aff29ac4241f250641f0cb1262e
Lucky1403 <lucky.230062289@gmail.com>
2026-08-14 16:36:34 +0530

    Solved Partition List Leetcode Problem

---
+++ 86_Partition List.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(-1);
+        ListNode dummy2 = new ListNode(-2);
+        ListNode t1 = dummy1;
+        ListNode t2 = dummy2;
+
+        ListNode temp = head;
+        while(temp != null)
+        {
+            if(temp.val < x)
+            {
+                t1.next = temp;
+                t1 = temp;
+                temp = temp.next;
+            }
+            else
+            {
+                t2.next = temp;
+                t2 = temp;
+                temp = temp.next;
+            }
+        }
+        t1.next = null;
+        t2.next = null;
+
+        t1.next = dummy2.next;
+
+        return dummy1.next;
+    }
+}
