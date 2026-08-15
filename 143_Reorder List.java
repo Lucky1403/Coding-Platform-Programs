5e02b043de8ab275c0781c6bb5bd045df2f41f1c
Lucky1403 <lucky.230062289@gmail.com>
2026-08-15 19:16:01 +0530

    Solved Reorder List Leetcode Problem

--- 143_Reorder List.java
+++ 143_Reorder List.java
@@ -1,20 +1,27 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode current = head;
         ListNode forward = null;
         ListNode previous = null;
-        while(current != null)
-        {
+        while (current != null) {
             forward = current.next;
             current.next = previous;
             previous = current;
@@ -28,38 +35,32 @@ class Solution {
         ListNode temp = head;
         int count = 0;
 
-        while(temp != null)
-        {
+        while (temp != null) {
             count++;
             temp = temp.next;
         }
 
         int half = 0;
-        if(count % 2 == 0)
-        {
+        if (count % 2 == 0) {
             half = count / 2;
-        }
-        else
-        {
-            half = (count + 1)/2;
+        } else {
+            half = (count + 1) / 2;
         }
         ListNode tempA = head;
         ListNode tempB = head;
         temp = head;
-        for(int i = 1; i < half; i++)
-        {
+        for (int i = 1; i < half; i++) {
             temp = temp.next;
         }
 
         tempB = temp.next;
         temp.next = null;
 
-        ListNode secondHalf = reverseList(tempB); 
+        ListNode secondHalf = reverseList(tempB);
         ListNode tempSecond = secondHalf;
 
         temp = head;
-        while(tempSecond != null)
-        {
+        while (tempSecond != null) {
             tempA = tempA.next;
             temp.next = tempSecond;
             temp = tempSecond;
@@ -70,4 +71,4 @@ class Solution {
         }
         return;
     }
-} 
+}
