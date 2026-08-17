9d51c36cfc7d279bbd17ace02f27093c57e125c4
Lucky1403 <lucky.230062289@gmail.com>
2026-08-17 13:26:07 +0530

    Solved Valid Parentheses Leetcode Problem

---
+++ 20_Valid Parentheses.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean sameStyle(char a, char b){
+        if(a == '[' && b == ']')
+            return true;
+        if(a == '(' && b == ')')
+            return true;
+        if(a == '{' && b == '}')
+            return true;
+        
+        return false;
+    }
+    public boolean isValid(String s) {
+        if(s.length() % 2 != 0)
+        {
+            return false;
+        }
+
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '[')
+            {
+                st.push(ch);
+            }
+            else
+            {
+                if(st.isEmpty())
+                {
+                    return false;
+                }
+                char top = st.peek();
+                if(sameStyle(top, ch))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return (st.size()==0);
+    }
+}
