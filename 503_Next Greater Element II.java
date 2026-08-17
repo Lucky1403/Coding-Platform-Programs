7b645dceff1ca0749186cecc8bd958cbb5e81308
Lucky1403 <lucky.230062289@gmail.com>
2026-08-17 21:08:32 +0530

    Solved Next Greater Element II Leetcode Problem

---
+++ 503_Next Greater Element II.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+        int n = nums.length;
+        for(int i = n - 1; i >= 0; i--)
+        {
+            st.push(nums[i]);
+        }
+
+        int[] ans = new int[n];
+        for(int i = n - 1; i >= 0; i--)
+        {
+            while(st.size() > 0 && nums[i] >= st.peek())
+            {
+                st.pop();
+            }
+            
+            if(st.isEmpty())
+            {
+                ans[i] = -1;
+                st.push(nums[i]);
+            }
+            else
+            {
+                ans[i] = st.peek();
+                st.push(nums[i]);
+            }
+        }
+        return ans;
+    }
+}
