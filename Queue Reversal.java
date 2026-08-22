d4553b7b6d6fa2e97505fd74bfd4552edb49dff3
Lucky1403 <lucky.230062289@gmail.com>
2026-08-23 01:25:56 +0530

    Solved Queue Reversal GFG Problem

---
+++ Queue Reversal.java
@@ -0,0 +1,17 @@
+import java.util.Queue;
+import java.util.Stack;
+
+class Solution {
+    public void reverseQueue(Queue<Integer> q) {
+        Stack<Integer> st = new Stack<>();
+        while(q.size() > 0)
+        {
+            st.push(q.remove());
+        }
+        
+        while(st.size() > 0)
+        {
+            q.add(st.pop());
+        }
+    }
+}
