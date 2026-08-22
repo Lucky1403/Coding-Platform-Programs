947d4897a092cd40cc62f37c7adf1e0174b18a2c
Lucky1403 <lucky.230062289@gmail.com>
2026-08-22 22:34:42 +0530

    Solved Find the Winner of the Circular Game Leetcode Problem

---
+++ 1823_Find the Winner of the Circular Game.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int findTheWinner(int n, int k) {
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 1; i <=n; i++)
+        {
+            q.add(i);
+        }
+
+        while(q.size() > 1)
+        {
+            for(int i = 1; i < k; i++)
+            {
+                q.add(q.remove());
+            }
+            q.remove();
+        }
+
+        int solution = 0;
+        for(int x : q)
+        {
+            solution = x;
+        }
+        return solution;
+    }
+}
