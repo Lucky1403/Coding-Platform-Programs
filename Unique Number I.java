6ce7a4abc333f5d94788e329f2ecb2abf3d4d2b6
Lucky1403 <lucky.230062289@gmail.com>
2026-08-24 16:22:51 +0530

    Solved Unique Number I GFG Problem

---
+++ Unique Number I.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findUnique(int[] arr) {
+        int result = 0;
+        for(int num : arr)
+        {
+            result ^= num;
+        }
+        return result;
+    }
+}
