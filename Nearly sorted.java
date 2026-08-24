eb23c50d87f1d1ae116902722d1c4bed3fab3c46
Lucky1403 <lucky.230062289@gmail.com>
2026-08-24 22:28:54 +0530

    Solved Nearly Sorted GFG Problem

---
+++ Nearly sorted.java
@@ -0,0 +1,21 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public void nearlySorted(int[] arr, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int idx = 0;
+        for(int ele : arr)
+        {
+            pq.add(ele);
+            if(pq.size() > k)
+            {
+                arr[idx++] = pq.remove();
+            }
+        }
+        
+        while(pq.size() > 0)
+        {
+            arr[idx++] = pq.remove();
+        }
+    }
+}
