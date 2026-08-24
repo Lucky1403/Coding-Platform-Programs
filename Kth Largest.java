f8983766f42fd4add1b22827de708b41bba5b6c7
Lucky1403 <lucky.230062289@gmail.com>
2026-08-24 21:41:35 +0530

    Solved Kth Largest GFG Problem

---
+++ Kth Largest.java
@@ -0,0 +1,16 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public static int kthLargest(int arr[], int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int ele : arr)
+        {
+            pq.add(ele);
+            if(pq.size() > k)
+            {
+                pq.remove();
+            }
+        }
+        return pq.peek();
+    }
+}
