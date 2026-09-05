fc1e74b5dbda7b6de06328fd001ef17709bff0b9
Lucky1403 <lucky.230062289@gmail.com>
2026-09-05 11:56:50 +0530

    Solved Smallest Stable Index II Leetcode Problem

---
+++ 3904_Smallest Stable Index II.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int n = nums.length;
+
+        int[] minArray = new int[n];
+        int[] maxArray = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] > max)
+            {
+                max = nums[i];
+                maxArray[i] = max;
+            }
+            maxArray[i] = max;
+        }
+
+        for(int i = n-1; i >= 0; i--)
+        {
+            if(nums[i] < min)
+            {
+                min = nums[i];
+                minArray[i] = min;
+            }
+            minArray[i] = min;
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            if(maxArray[i] - minArray[i] <= k)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
