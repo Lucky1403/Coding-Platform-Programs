a9fda3f8910b640c0b61965a234410e17edf5b2f
Lucky1403 <lucky.230062289@gmail.com>
2026-09-05 12:23:35 +0530

    Solved Diamater of a Binary Tree GFG Problem

---
+++ Diameter of a Binary Tree.java
@@ -0,0 +1,29 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+};
+
+class Solution {
+    static int max;
+    
+    public int diameter(Node root) {
+        max = 0;
+        levels(root);
+        return max;
+    }
+    
+    public int levels(Node root)
+    {
+        if(root == null)
+            return 0;
+        int leftLevels = levels(root.left);
+        int rightLevels = levels(root.right);
+        max = Math.max(max, leftLevels + rightLevels);
+        return 1 + Math.max(leftLevels, rightLevels);
+    }
+}
