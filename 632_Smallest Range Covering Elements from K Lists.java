48bab73462ffbb4a258d30de810d2dbaa885b057
Lucky1403 <lucky.230062289@gmail.com>
2026-09-12 17:31:46 +0530

    Solved Smallest Range Covering Elements from K Lists Leetcode Problem

---
+++ 632_Smallest Range Covering Elements from K Lists.java
@@ -0,0 +1,58 @@
+import java.util.List;
+import java.util.PriorityQueue;
+
+class Solution {
+    class Triplet implements Comparable<Triplet>{
+        int value;
+        int rowIndex;
+        int columnIndex;
+
+        Triplet(int value, int rowIndex, int columnIndex)
+        {
+            this.value = value;
+            this.rowIndex = rowIndex;
+            this.columnIndex = columnIndex;
+        }
+
+        public int compareTo(Triplet other)
+        {
+            return Integer.compare(this.value, other.value);
+        }
+    }
+    
+    public int[] smallestRange(List<List<Integer>> nums) {
+        PriorityQueue<Triplet> minHeap = new PriorityQueue<>();
+        
+        int maximum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int val = nums.get(i).get(0);
+            minHeap.add(new Triplet(val, i, 0));
+            maximum = Math.max(maximum, val);
+        }
+
+        int start = minHeap.peek().value;
+        int end = maximum;
+
+        while(minHeap.size() == nums.size())
+        {
+            Triplet current = minHeap.remove();
+            int row = current.rowIndex;
+            int col = current.columnIndex;
+            if(maximum - current.value < end - start)
+            {
+                start = current.value;
+                end = maximum;
+            }
+
+            if(col + 1 < nums.get(row).size()){
+                int nextValue = nums.get(row).get(col + 1);
+                minHeap.add(new Triplet(nextValue, row, col + 1));
+                maximum = Math.max(nextValue, maximum);
+            }
+            else
+                break;
+        }
+        return new int[]{start, end};
+    }
+}
