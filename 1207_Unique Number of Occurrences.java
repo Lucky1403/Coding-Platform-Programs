5d0b71d1f87739197654a66706f540a43ac883d7
Lucky1403 <lucky.230062289@gmail.com>
2026-09-19 22:42:59 +0530

    Solved Unique Number of Occurences Leetcode Problem

---
+++ 1207_Unique Number of Occurrences.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++)
+        {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) +1);
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        for(int key : map.keySet())
+        {
+            set.add(map.get(key));
+        }
+
+        if(map.size() == set.size())
+            return true;
+        else
+            return false;
+    }
+}
