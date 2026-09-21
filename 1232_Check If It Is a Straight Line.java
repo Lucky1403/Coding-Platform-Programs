66dbbeb834d98f510cc6a6a1ade976fcc5479df8
Lucky1403 <lucky.230062289@gmail.com>
2026-09-21 23:58:58 +0530

    Refactored Check if it is a Straight line Leetcode Problem

--- 1232_Check If It Is a Straight Line.java
+++ 1232_Check If It Is a Straight Line.java
@@ -4,14 +4,14 @@ class Solution {
         int x0 = coordinates[0][0], y0 = coordinates[0][1];
         int x1 = coordinates[1][0], y1 = coordinates[1][1];
         
-        int dx = x1 - x0;
-        int dy = y1 - y0;
-
+        int m1 = x1 - x0;
+        int m2 = y1 - y0;
+        //Calculating the slope
         for (int i = 2; i < coordinates.length; i++) {
             int x = coordinates[i][0];
             int y = coordinates[i][1];
 
-            if (dy * (x - x0) != dx * (y - y0)) {
+            if (m2 * (x - x0) != m1 * (y - y0)) {
                 return false;
             }
         }
