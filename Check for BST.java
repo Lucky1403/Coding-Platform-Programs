f5b9f2462ab4bf5c527e13d42a38f8df397360b6
Lucky1403 <lucky.230062289@gmail.com>
2026-09-26 15:09:27 +0530

    Solved Check for BST GFG Problem

---
+++ Check for BST.java
@@ -0,0 +1,34 @@
+/* Structure of a Binary Search Tree node
+class Node {
+	int data;
+	Node left;
+	Node right;
+	
+	Node(int val) {
+		data = val;
+		left = right = null;
+	}
+} */
+
+import java.util.ArrayList;
+
+class Solution {
+	public boolean isBST(Node root) {
+		ArrayList<Integer> list = new ArrayList<>();
+		inOrder(root, list);
+		for (int i = 1; i < list.size(); i++)
+			{
+			if (list.get(i - 1) >= list.get(i))
+				return false;
+		}
+		return true;
+	}
+	
+	public void inOrder(Node root, ArrayList<Integer> list) {
+		if (root == null)
+			return;
+		inOrder(root.left, list);
+		list.add(root.data);
+		inOrder(root.right, list);
+	}
+}
